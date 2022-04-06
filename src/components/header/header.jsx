cedede1c282b759ca2e10590e866ce5bb6b56337
Edward Toonela <edward.toonela@icloud.com>
2022-04-06 16:21:53 -0300

    added header w/ greeting, cv download button and lets talk

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Edward Toonela</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
   )
 }
 
