7ed01be5c09c3796180790434bb89790914853bc
Edward Toonela <edward.toonela@icloud.com>
2022-04-06 18:09:32 -0300

    cta, me and header socials css

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsYoutube} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div>
+    <div className="header__socials">
       <a href="https://www.linkedin.com/in/edward-toonela/" target="_blank"> <BsLinkedin /> </a>
-      <a href="https://www.github.com" target="_blank"> <BsGithub /> </a>
-      <a href="https://www.youtube.com" target="_blank"> <BsYoutube /> </a>
+      <a href="https://github.com/Edwardtoonela" target="_blank"> <BsGithub /> </a>
+      <a href="https://www.instagram.com/edwardtoonela/" target="_blank"> <BsInstagram /> </a>
     </div>
   )
 }
