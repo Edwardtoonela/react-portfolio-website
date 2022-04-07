5c4b6f0c62d863d74cb1820aace9e6de6f9d1087
Edward Toonela <edward.toonela@icloud.com>
2022-04-07 10:41:20 -0300

    added portfolio projects with placeholder pictures and gh + live links

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This Portfolio Page</h3>
+          <a href="https://github.com/Edwardtoonela/react-portfolio-website" className="btn" target='_blank'>Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio_item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Travel Tribe</h3>
+          <a href="https://github.com/Edwardtoonela/travel-tribe" className="btn" target='_blank'>Github</a>
+          <a href="http://www.trvltribe.com/" className="btn btn-primary" target='_blank'>Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio_item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>Airbnb clone "Trippy Boats"</h3>
+          <a href="https://github.com/dancosta-fed/trippy-boats" className="btn" target='_blank'>Github</a>
+          <a href="https://trippy-boats.herokuapp.com/" className="btn btn-primary" target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
