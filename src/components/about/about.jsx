d578f07b155dc70f8ee0b886ba96f07cff8fbcaa
Edward Toonela <edward.toonela@icloud.com>
2022-04-06 23:40:30 -0300

    completed about me picture csslayout

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -13,8 +13,9 @@ function About() {
 
       <div className="container about__container">
         <div className="about__me">
-          <div className="about__me-image"></div>
-          <img src={ME} alt="About Image" />
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
         </div>
 
         <div className="about__content">
