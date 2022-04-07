2ed82d3bdf7e41107d15ebca4043b7276f787d38
Edward Toonela <edward.toonela@icloud.com>
2022-04-07 11:35:46 -0300

    hardcoded vers of testm ready

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR from '../../assets/comp2.png'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section>
+      <h5>Review from clients and coworkers</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              He is so nice bla bla bla
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              He is so nice bla bla bla
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar One" />
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              He is so nice bla bla bla
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
