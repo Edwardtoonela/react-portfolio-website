49c1ee9c22c68d67c3f3762bee808e02809e991e
Edward Toonela <edward.toonela@icloud.com>
2022-04-07 08:55:52 -0300

    listed experienced front-back

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience front__end">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Experienced</small>
+              <h4>HTML</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>CSS</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>Javascript</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>React</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>Figma</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Experienced</small>
+              <h4>Sass</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>Git</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience back__end">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Experienced</small>
+              <h4>Ruby on Rails</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Intermediate</small>
+              <h4>Node JS</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Experienced</small>
+              <h4>Heroku</h4>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <small>Beginner</small>
+              <h4>MySQL</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
