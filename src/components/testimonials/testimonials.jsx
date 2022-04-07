2da1317275f66520d801fc593aa71b5f4389ab67
Edward Toonela <edward.toonela@icloud.com>
2022-04-07 12:10:32 -0300

    dynamic reviews

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import './testimonials.css'
 import AVTR from '../../assets/comp2.png'
 
+const data = [
+  {
+    avatar: AVTR,
+    name: 'John Doe',
+    review: 'He is so nice bla bla bla'
+  },
+  {
+    avatar: AVTR,
+    name: 'John Doe',
+    review: 'Hdkas asd asd öasfdölasd l'
+  },
+  {
+    avatar: AVTR,
+    name: 'John Doe',
+    review: 'He is so nice bla bla bla'
+  },
+  {
+    avatar: AVTR,
+    name: 'John Doe',
+    review: 'He is so nice bla bla bla'
+  },
+]
+
 const Testimonials = () => {
   return (
     <section>
@@ -9,33 +32,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar One" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            He is so nice bla bla bla
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar One" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            He is so nice bla bla bla
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar One" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            He is so nice bla bla bla
-          </small>
-        </article>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar}/>
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
