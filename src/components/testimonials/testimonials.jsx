64459947f0af69445d3558a189fa5ef6e399afb3
Edward Toonela <edward.toonela@icloud.com>
2022-04-27 16:12:24 +0000

    Testimonials finished

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -19,13 +19,21 @@ const data = [
     avatar: AVTR3,
     name: 'Carla Valdivia',
     review: `It is my pleasure to recommend Edward Toonela for a position as web developer.
+
     During #822 batch, Edward displayed great talents in technical applications such as Ruby, SQL, Ruby on Rails, HTML, CSS, Javascript and Figma.
+
     I truly believe he’ll be a great addition to any company looking for a developer.`
   },
   {
     avatar: AVTR4,
     name: 'Daniel Costa',
-    review: `I met Ed at Le Wagon Rio, we've done a Full Stack Rails Development Bootcamp together. And at the end of the course, we worked together for the two final projects we had. One of which, was Ed's idea (Travel Tribe). I was happy and lucky to work with him. He's very driven and easy to work with. Has a good mindset, creative and smart when it comes to programming logic. Whatever he set himself to do, he did it really well. I'd be happy to work with people like Ed.`
+    review: `I met Ed at Le Wagon Rio, we've done a Full Stack Rails Development Bootcamp together.
+
+    And at the end of the course, we worked together for the two final projects we had. One of which, was Ed's idea (Travel Tribe). I was happy and lucky to work with him.
+
+    He's very driven and easy to work with. Has a good mindset, creative and smart when it comes to programming logic. Whatever he set himself to do, he did it really well.
+
+    I'd be happy to work with people like Ed.`
   },
 ]
 
